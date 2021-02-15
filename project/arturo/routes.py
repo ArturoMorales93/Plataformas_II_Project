c7516405f4e701b3a0b035287db36a59921f5df1
ArturoMorales93 <62071280+ArturoMorales93@users.noreply.github.com>
2021-02-15 16:21:47 -0600

    Upgrade UI (#3)
    * Se optimiza el código
    
    * Ajustes de las páginas secundarias
    
    * Ajustes del layout para páginas de los temas

--- project/arturo/routes.py
+++ project/arturo/routes.py
@@ -1,6 +1,8 @@
 from flask import render_template
 from . import arturo
+# Especificar el tema en la variable
+title = "Machine Learning"
 
-@arturo.route('/arturo', methods=['GET'])
+@arturo.route('/machine-learning', methods=['GET'])
 def arturo():
-    return render_template('index_arturo.html')
+    return render_template('machine-learning.html', title=title)
