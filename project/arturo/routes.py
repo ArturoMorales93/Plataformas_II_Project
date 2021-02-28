26c8094072efcc3354d6185ec30107d342269db4
Carlos Mairena <carlosmiguelmairena@gmail.com>
2021-02-28 13:28:23 -0600

    Propuesta 1

--- project/arturo/routes.py
+++ project/arturo/routes.py
@@ -1,8 +1,8 @@
 from flask import render_template
 from . import arturo
-# Especificar el tema en la variable
-title = "Machine Learning"
 
 @arturo.route('/machine-learning', methods=['GET'])
 def arturo():
+    # Especificar el tema en la variable
+    title = "Machine Learning"
     return render_template('machine-learning.html', title=title)
