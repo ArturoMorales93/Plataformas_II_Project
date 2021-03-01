00fc660f62f7167398228e373adb4fc0d7b27143
Carlos Mairena <44934161+carlosmmairena@users.noreply.github.com>
2021-02-28 19:07:11 -0600

    Feature google translate (#5)
    * Implementacion del traductor
    
    * Add /iot route
    
    * Correcciones en html de /iot
    
    * Propuesta 1

--- project/carlos/routes.py
+++ project/carlos/routes.py
@@ -1,6 +1,12 @@
 from flask import render_template
 from . import carlos
 
-@carlos.route('/carlos', methods=['GET'])
-def carlos():
-    return render_template('index_carlos.html')
+
+@carlos.route('/es/iot', methods=['GET'])
+def iot_es():
+    return render_template('es_iot.html')
+
+
+@carlos.route('/en/iot', methods=['GET'])
+def iot_en():
+    return render_template('en_iot.html')
