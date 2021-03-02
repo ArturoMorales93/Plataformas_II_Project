33dc88d53fb6db5bf05027a6b92cd3889d49b57e
Carlos Mairena <carlosmiguelmairena@gmail.com>
2021-03-01 21:51:25 -0600

    Templates con idioma ingles y espanol

--- project/arturo/routes.py
+++ project/arturo/routes.py
@@ -1,8 +1,16 @@
 from flask import render_template
 from . import arturo
 
+@arturo.route('/es/machine-learning', methods=['GET'])
 @arturo.route('/machine-learning', methods=['GET'])
-def arturo():
+def es_arturo():
     # Especificar el tema en la variable
     title = "Machine Learning"
     return render_template('machine-learning.html', title=title)
+
+
+@arturo.route('/en/machine-learning', methods=['GET'])
+def en_arturo():
+    # Especificar el tema en la variable
+    title = "Machine Learning"
+    return render_template('en_machine-learning.html', title=title)
--- project/dixiana/routes.py
+++ project/dixiana/routes.py
@@ -1,6 +1,12 @@
 from flask import render_template
 from . import dixiana
 
-@dixiana.route('/dixiana', methods=['GET'])
-def dixiana():
-    return render_template('index_dixiana.html')
+@dixiana.route('/es/tecnologia-5g', methods=['GET'])
+@dixiana.route('/tecnologia-5g', methods=['GET'])
+def es_dixiana():
+    return render_template('es_index_dixiana.html')
+
+
+@dixiana.route('/en/tecnologia-5g', methods=['GET'])
+def en_dixiana():
+    return render_template('en_index_dixiana.html')
