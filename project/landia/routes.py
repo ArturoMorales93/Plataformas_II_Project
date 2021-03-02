8c98840c07c71bd768014c1d1ef8cf8663ca54ee
Carlos Mairena <44934161+carlosmmairena@users.noreply.github.com>
2021-03-01 21:53:54 -0600

    Templates con idioma ingles y espanol (#8)

--- project/landia/routes.py
+++ project/landia/routes.py
@@ -1,6 +1,12 @@
 from flask import render_template
 from . import landia
 
-@landia.route('/landia', methods=['GET'])
-def landia():
-    return render_template('index_landia.html')
+@landia.route('/es/nanotecnologia', methods=['GET'])
+@landia.route('/nanotecnologia', methods=['GET'])
+def es_landia():
+    return render_template('es_index_landia.html')
+
+
+@landia.route('/en/nanotecnologia', methods=['GET'])
+def en_landia():
+    return render_template('en_index_landia.html')
