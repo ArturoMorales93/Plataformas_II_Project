fb5fc576a821d2750d55d295fca115a26e52875b
Carlos Mairena <carlosmiguelmairena@gmail.com>
2021-04-12 15:19:38 -0600

    Inicio de descripcion de MDM

--- project/mdm/routes.py
+++ project/mdm/routes.py
@@ -1,6 +1,7 @@
 from flask import render_template
 from . import mdm
 
+@mdm.route('/en/mdm', methods=['GET'])
 @mdm.route('/mdm', methods=['GET'])
 def mdm():
     return render_template('Master_Data_Management.html')
